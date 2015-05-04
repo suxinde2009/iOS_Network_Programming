08c13400fe31d23de48e9c84d344fac180cb8e79
sxd <suxinde2009@126.com>
2015-05-05 01:29:58 +0800

    Init

---
+++ AFNetworkingTestDemo/AFNetworkingTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingTestDemo
+//
+//  Created by su xinde on 15/5/5.
+//  Copyright (c) 2015年 su xinde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AFNetworkingTestDemo/AFNetworkingTestDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AFNetworkingTestDemo
+//
+//  Created by su xinde on 15/5/5.
+//  Copyright (c) 2015年 su xinde. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
